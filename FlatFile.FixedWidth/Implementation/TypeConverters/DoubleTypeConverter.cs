83fda4f02889c92553b8f7946794c8fb81b81fae
Dan Mohr <danmohr@live.com>
2018-08-08 09:33:05 -0400

    Added comments

--- FlatFile.FixedWidth/Implementation/TypeConverters/DoubleTypeConverter.cs
+++ FlatFile.FixedWidth/Implementation/TypeConverters/DoubleTypeConverter.cs
@@ -6,6 +6,9 @@ namespace FlatFile.FixedWidth.Implementation.TypeConverters
     {
         public object ConvertFromString(string stringValue)
         {
+            // This currently fails for double.MaxValue.
+            // Can reproduce here: double.Parse(double.MaxValue.ToString());
+            // Seems strange that the documented maximum value throws OverflowException: Value was either too large or too small for a Double
             return double.Parse(stringValue.Trim());
         }
     }
