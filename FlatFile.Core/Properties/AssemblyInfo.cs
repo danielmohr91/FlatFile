75246624a8fab665c93cd97d1016ea283ee22eee
Dan Mohr <danmohr@live.com>
2018-09-25 09:05:51 -0400

    Updated build information

--- FlatFile.Core/Properties/AssemblyInfo.cs
+++ FlatFile.Core/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FlatFile.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Flat File Parser")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FlatFile.Core")]
